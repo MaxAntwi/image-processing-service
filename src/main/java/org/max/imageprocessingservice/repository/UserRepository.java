ecca891a9bbd4ff36da5283a4904d705f7a2ea4d
Maxwell Antwi <maxwell.antwi@amalitech.com>
2024-08-27 11:19:31 +0100

    feat: security config

--- src/main/java/org/max/imageprocessingservice/repository/UserRepository.java
+++ src/main/java/org/max/imageprocessingservice/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.max.imageprocessingservice.repository;
 import org.max.imageprocessingservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
